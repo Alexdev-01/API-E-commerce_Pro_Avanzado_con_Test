60934353cb2e95959257b6cb00a68303e35b2ef8
Alexdev-01 <colladoacs@gmail.com>
2026-05-25 15:12:00 +0200

    commit

---
+++ src/main/java/com/tiendaonline/gestion/ApiGestionProductosYPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaonline.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionProductosYPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionProductosYPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaonline/gestion/ApiGestionProductosYPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaonline.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionProductosYPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
