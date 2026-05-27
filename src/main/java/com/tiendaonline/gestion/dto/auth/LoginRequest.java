a55eb8c7ed86ae66ff5c81828173bc7f5760ccf5
Alexdev-01 <colladoacs@gmail.com>
2026-05-27 15:41:22 +0200

    commit

---
+++ src/main/java/com/tiendaonline/gestion/dto/auth/LoginRequest.java
@@ -0,0 +1,35 @@
+package com.tiendaonline.gestion.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+
+// Solicitud de inicio de sesión (login) en la aplicación.
+public class LoginRequest {
+	
+	@NotBlank  // Asegura que el campo no esté vacío o solo contenga espacios en blanco
+	private String username;
+	
+	@NotBlank
+	private String password;
+
+	public LoginRequest() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+
+}
