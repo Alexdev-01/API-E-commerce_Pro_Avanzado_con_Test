a5ccc794b9e86a356f27042e1436daa156b5e38b
Alexdev-01 <colladoacs@gmail.com>
2026-05-31 15:43:22 +0200

    commit

---
+++ src/main/java/com/tiendaonline/gestion/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.tiendaonline.gestion.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tiendaonline.gestion.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	Optional<Categoria> findByNombre(String nombre);
+
+}
