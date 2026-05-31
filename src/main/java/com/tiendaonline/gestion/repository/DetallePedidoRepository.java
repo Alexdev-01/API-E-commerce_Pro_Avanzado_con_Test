68cf74d46155e019eda2b79d0d8caeea26b99728
Alexdev-01 <colladoacs@gmail.com>
2026-05-31 15:43:35 +0200

    commit

---
+++ src/main/java/com/tiendaonline/gestion/repository/DetallePedidoRepository.java
@@ -0,0 +1,9 @@
+package com.tiendaonline.gestion.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tiendaonline.gestion.model.DetallePedido;
+
+public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Long> {
+
+}
