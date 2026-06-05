7c8ce628c18524d47b501f814a56ed7e7b5c3e6d
Alexdev-01 <colladoacs@gmail.com>
2026-06-05 21:18:44 +0200

    commit

---
+++ src/main/java/com/tiendaonline/gestion/service/PedidoService.java
@@ -0,0 +1,22 @@
+package com.tiendaonline.gestion.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.tiendaonline.gestion.dto.pedido.CrearPedidoRequest;
+import com.tiendaonline.gestion.dto.pedido.PedidoResponse;
+import com.tiendaonline.gestion.model.Pedido;
+import com.tiendaonline.gestion.model.Usuario;
+
+public interface PedidoService {
+	
+	Pedido crearPedido(Pedido pedido);
+	
+	List<PedidoResponse> obtenerPedidosUsuario(Usuario usuario);
+	
+	List<PedidoResponse> obtenerTodos();
+	
+	ResponseEntity<PedidoResponse> crearPedido(CrearPedidoRequest request, String username);
+
+}
