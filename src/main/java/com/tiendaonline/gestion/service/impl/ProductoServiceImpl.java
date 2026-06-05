1c34fee0327ccc293f23fca7460c9bee7bad55a3
Alexdev-01 <colladoacs@gmail.com>
2026-06-05 21:18:34 +0200

    commit

---
+++ src/main/java/com/tiendaonline/gestion/service/impl/ProductoServiceImpl.java
@@ -0,0 +1,103 @@
+package com.tiendaonline.gestion.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.tiendaonline.gestion.model.Producto;
+import com.tiendaonline.gestion.repository.ProductoRepository;
+import com.tiendaonline.gestion.service.ProductoService;
+import org.springframework.stereotype.Service;
+
+import com.tiendaonline.gestion.dto.producto.ProductoResponse;
+import com.tiendaonline.gestion.exception.ResourceNotFoundException;
+
+
+
+@Service	// Marca esta clase como un componente de servicio en Spring
+// Implementa la interfaz ProductoService para proporcionar la lógica de negocio relacionada con los productos
+public class ProductoServiceImpl implements ProductoService{
+
+	private final ProductoRepository productoRepository;
+
+	public ProductoServiceImpl(ProductoRepository productoRepository) {
+		this.productoRepository = productoRepository;
+	}
+
+	@Override
+	public Producto crearProducto(Producto producto) {
+		return productoRepository.save(producto);
+	}
+
+	// Método auxiliar interno para obtener la entidad Producto (no el DTO)
+	private Producto obtenerEntidadPorId(Long id) {
+		return productoRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Producto no encontrado con id: " + id));
+	}
+
+	@Override
+	public Producto actualizarProducto(Long id, Producto producto) {
+		Producto existente = obtenerEntidadPorId(id);
+
+		existente.setNombre(producto.getNombre());
+		existente.setDescripcion(producto.getDescripcion());
+		existente.setPrecio(producto.getPrecio());
+		existente.setStock(producto.getStock());
+		existente.setCategoria(producto.getCategoria());
+
+		return productoRepository.save(existente);
+	}
+
+	@Override
+	public void eliminarProducto(Long id) {
+		Producto producto = obtenerEntidadPorId(id);
+		producto.setActivo(false); 
+		productoRepository.save(producto);
+	}
+
+	@Override
+	public ProductoResponse obtenerPorId(Long id) {
+		Producto producto = productoRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Producto no encontrado con id: " + id));
+		return mapToResponse(producto);
+	}
+
+	@Override
+	public List<ProductoResponse> listarTodos() {
+		return productoRepository.findAll()
+				.stream().filter(Producto::getActivo).map(this::mapToResponse).toList();
+	}
+	
+	// Método privado para mapear un objeto Producto a un ProductoResponse
+	private ProductoResponse mapToResponse(Producto producto) {
+		return new ProductoResponse(
+				producto.getId(), producto.getNombre(), producto.getDescripcion(),producto.getPrecio(),
+				producto.getStock(), producto.getCategoria() != null? producto.getCategoria().getNombre() : null);
+	}
+
+	@Override
+	public Page<ProductoResponse> listarProductosPaginados(int page, int size) {
+		
+		//todos los productos
+		//Page<Producto> producto = productoRepository.findAll(PageRequest.of(page, size));
+		
+		//solo los productos activos
+		Page<Producto> producto = productoRepository.findByActivoTrue(PageRequest.of(page, size));
+		
+		return producto.map(this::mapToResponse);
+	}
+
+	@Override
+	public Page<ProductoResponse> filtrarPrductos(Long categoriaId, BigDecimal precioMin, BigDecimal precioMax, int page, int size) {
+		Page<Producto> producto = productoRepository.filtrarProductos(categoriaId, precioMin, precioMax, PageRequest.of(page, size));
+		return producto.map(this::mapToResponse);
+	}
+	
+	
+	
+	
+
+}
