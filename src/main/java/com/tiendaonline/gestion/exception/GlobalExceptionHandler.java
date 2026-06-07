859c658c713af24a1215c628ce797313fd599f19
Alexdev-01 <colladoacs@gmail.com>
2026-06-07 15:27:39 +0200

    commit

--- src/main/java/com/tiendaonline/gestion/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tiendaonline/gestion/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.gestiontareas.todolist.exception;
+package com.tiendaonline.gestion.exception;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -6,11 +6,9 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -21,12 +19,11 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)	// Indica que este método maneja excepciones del tipo ResourceNotFoundException
 	// Este método captura las excepciones de tipo ResourceNotFoundException, crea una respuesta de error con detalles y devuelve una respuesta con el estado HTTP 404 (Not Found)
 	public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
-		ErrorResponse error = new ErrorRespons(
+		ErrorResponse error = new ErrorResponse(
 				LocalDateTime.now(),
 				HttpStatus.NOT_FOUND.value(),
 				HttpStatus.NOT_FOUND.name(),
-				ex.getMessage(),
-				request.getRequestURI());
+				ex.getMessage());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
@@ -39,8 +36,7 @@ public class GlobalExceptionHandler {
 				LocalDateTime.now(),
 				HttpStatus.INTERNAL_SERVER_ERROR.value(),
 				HttpStatus.INTERNAL_SERVER_ERROR.name(),
-				"Error interno el servidor: ",
-				request.getRequestURI());
+				"Error interno el servidor: ");
 		
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
 				
@@ -62,23 +58,10 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(BadRequestException.class)	// Indica que este método maneja excepciones del tipo BadRequestException, que se lanzan cuando hay un error de solicitud por parte del cliente
 	// Este método captura las excepciones de tipo BadRequestException, crea una respuesta de error con detalles y devuelve una respuesta con el estado HTTP 400 (Bad Request)
 	public ResponseEntity<ErrorResponse> handleBadRequest(BadRequestException ex, HttpServletRequest request) {
-		ErrorResponse error = new ErrorResponse(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name(), ex.getMessage(), request.getRequestURI());
+		ErrorResponse error = new ErrorResponse(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name(), ex.getMessage());
 		
 		return ResponseEntity.badRequest().body(error);
 	}
 	
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)	// Indica que este método maneja excepciones del tipo MethodArgumentNotValidException, que se lanzan cuando la validación de los argumentos de un método falla
-	// Este método captura las excepciones de validación, extrae los errores de validación y devuelve una respuesta con un mapa de campos y mensajes de error
-	public ResponseEntity<Map<String, String>> manejarValidaciones(
-	        MethodArgumentNotValidException ex) {
 
-	    Map<String, String> errores = new HashMap<>();
-
-	    ex.getBindingResult().getFieldErrors().forEach(error ->
-	            errores.put(error.getField(), error.getDefaultMessage())
-	    );
-
-	    return ResponseEntity.badRequest().body(errores);
-	}
 }
