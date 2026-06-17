97366c68a2d927db490e07c77b2267fd87208f19
Alexdev-01 <colladoacs@gmail.com>
2026-06-17 15:08:28 +0200

    commit

--- src/main/java/com/tiendaonline/gestion/dto/producto/ProductoResponse.java
+++ src/main/java/com/tiendaonline/gestion/dto/producto/ProductoResponse.java
@@ -31,20 +31,38 @@ public class ProductoResponse {
 	public Long getId() {
 		return id;
 	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getNombre() {
 		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 	public String getDescripcion() {
 		return descripcion;
 	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 	public BigDecimal getPrecio() {
 		return precio;
+	}
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
 	}
 	public Integer getStock() {
 		return stock;
 	}
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
 	public String getCategoriaNombre() {
 		return categoriaNombre;
+	}
+	public void setCategoriaNombre(String categoriaNombre) {
+		this.categoriaNombre = categoriaNombre;
 	}
 	
 	
