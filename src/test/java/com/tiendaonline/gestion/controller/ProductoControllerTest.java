70504c2bfbf7ab194704406d79674ac45be706c4
Alexdev-01 <colladoacs@gmail.com>
2026-06-17 15:08:38 +0200

    commit

--- src/test/java/com/tiendaonline/gestion/controller/ProductoControllerTest.java
+++ src/test/java/com/tiendaonline/gestion/controller/ProductoControllerTest.java
@@ -2,12 +2,15 @@ package com.tiendaonline.gestion.controller;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -31,12 +34,44 @@ public class ProductoControllerTest {
 	@Test
 	void deberiaListarProductos() throws Exception {
 		
-		Page<ProductoResponse> pagina = new PageImpl<>(Collections.emptyList());
+		List<ProductoResponse> productos = List.of(new ProductoResponse());
+		
+		when(productoService.listarTodos()).thenReturn(productos);
+
+		mockMvc.perform(get("/productos"))
+		.andExpect(status().isOk());
+	}
+	
+	// Test para verificar que el endpoint de obtener producto por ID devuelve un estado HTTP 200 OK
+	@Test
+	void deberiaObtenerProductoPorId() throws Exception {
+		
+		ProductoResponse producto = new ProductoResponse();
+		producto.setId(1L);
+		producto.setNombre("Laptop");
+		
+		when(productoService.obtenerPorId(1L))
+			.thenReturn(producto);
+		
+		mockMvc.perform(get("/productos/1"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id").value(1))
+			.andExpect(jsonPath("$.nombre").value("Laptop"));
+	}
+	
+	// Test para verificar que el endpoint de listar productos paginados devuelve un estado HTTP 200 OK
+	@Test
+	void deberiaListarProductosPaginados() throws Exception {
+		
+		Page<ProductoResponse> pagina =new PageImpl<>(Collections.emptyList());
 		
 		when(productoService.listarProductosPaginados(0,5)).thenReturn(pagina);
 		
-	    mockMvc.perform(get("/productos"))
-	    .andExpect(status().isOk());
+		mockMvc.perform(get("/productos/paginado")
+				.param("page", "0")
+				.param("size", "5")
+		)
+		.andExpect(status().isOk());
 	}
 
 }
