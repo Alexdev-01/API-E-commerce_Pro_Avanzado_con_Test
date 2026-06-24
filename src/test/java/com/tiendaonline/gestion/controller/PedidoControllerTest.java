c0b7e29fa4294f182f340f6dc48efca6379ae411
Alexdev-01 <colladoacs@gmail.com>
2026-06-24 14:31:46 +0200

    commit

---
+++ src/test/java/com/tiendaonline/gestion/controller/PedidoControllerTest.java
@@ -0,0 +1,30 @@
+package com.tiendaonline.gestion.controller;
+
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(PedidoController.class)
+public class PedidoControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockitoBean
+	private PedidoService pedidoService;
+
+	
+	
+	
+
+}
