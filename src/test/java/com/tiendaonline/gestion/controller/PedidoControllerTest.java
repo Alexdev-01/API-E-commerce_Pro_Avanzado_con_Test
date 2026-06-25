f1e284f82481295b4cc484078226d11e7480b9d2
Alexdev-01 <colladoacs@gmail.com>
2026-06-25 15:27:37 +0200

    commit

--- src/test/java/com/tiendaonline/gestion/controller/PedidoControllerTest.java
+++ src/test/java/com/tiendaonline/gestion/controller/PedidoControllerTest.java
@@ -1,5 +1,6 @@
 package com.tiendaonline.gestion.controller;
 
+import static org.hamcrest.CoreMatchers.any;
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -7,12 +8,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.tiendaonline.gestion.dto.pedido.CrearPedidoRequest;
+import com.tiendaonline.gestion.dto.pedido.PedidoResponse;
+import com.tiendaonline.gestion.service.PedidoService;
 
 @WebMvcTest(PedidoController.class)
 public class PedidoControllerTest {
@@ -25,6 +32,29 @@ public class PedidoControllerTest {
 
 	
 	
-	
+	@Test
+	void deberiaCrearPedido() throws Exception {
+		
+		PedidoResponse response = new PedidoResponse();
+		
+		when(pedidoService.crearPedido(
+				any(CrearPedidoRequest.class),
+				anyString()))
+			.thenReturn(ResponseEntity.ok(response));
+		
+		mockMvc.perform(post("/pedidos")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("""
+						{
+							"items":[
+								{
+									"productoId":1,
+									"cantidad":2
+								}
+							]
+						}
+						"""))
+				.andExpect(status().isOk());
+	}
 
 }
